f63349dadcae620fbdda58432751342ffbe2f3a8
fyrdahl <alex@8bitsofsushi.com>
2013-11-16 20:55:37 +0100

    Updates

--- src/Entity.java
+++ src/Entity.java
@@ -1,8 +1,11 @@
 public class Entity {
 	
-	// An entity is any game object
+	// An entity is any game object 
+	// that has a position in the game world
+	
 	private int x;
 	private int y;
+	private boolean isSolid; // Probably useful for collision detection
 	
 	public int getX() {
 		return x;
@@ -10,12 +13,17 @@ public class Entity {
 	public void setX(int x) {
 		this.x = x;
 	}
-	
 	public int getY() {
 		return y;
 	}
 	public void setY(int y) {
 		this.y = y;
+	}
+	public boolean isSolid() {
+		return isSolid;
+	}
+	public void setSolid(boolean isSolid) {
+		this.isSolid = isSolid;
 	}
 	
 }
