b4aa743975e9b04127819fda25c6f75892e69c83
fyrdahl <alex@8bitsofsushi.com>
2013-11-16 20:47:15 +0100

    Some more comments

--- src/Platformer.java
+++ src/Platformer.java
@@ -8,7 +8,7 @@ import org.lwjgl.opengl.GL11;
 
 public class Platformer {
 	
-	// Setting a state variable
+	// Game-state flag
 	private boolean running = true;
 	
 	public void initOpenGL(String windowTitle, int width, int height)
@@ -17,37 +17,39 @@ public class Platformer {
 		{
 			DisplayMode[] dm = Display.getAvailableDisplayModes();
             for (DisplayMode mode : dm) {
-                    if (mode.getHeight() == height && mode.getWidth() == width
-                                    && mode.getBitsPerPixel() == 32) {
+                    if (mode.getHeight() == height && mode.getWidth() == width && mode.getBitsPerPixel() == 32) {
                             Display.setDisplayMode(mode);
                     }
             }
+            
             Display.setTitle(windowTitle);
             Display.create();
 
-            GL11.glMatrixMode(GL11.GL_PROJECTION);
+            // Set the projection mode
             GL11.glLoadIdentity();
-
+            GL11.glMatrixMode(GL11.GL_PROJECTION);
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
             GL11.glOrtho(0, Display.getDisplayMode().getWidth(), 0, Display.getDisplayMode().getHeight(), 1, -1);
 
-            GL11.glMatrixMode(GL11.GL_MODELVIEW);
-            GL11.glLoadIdentity();
-
+            
+            // Some more OpenGL settings
             GL11.glEnable(GL11.GL_ALPHA_TEST);
             GL11.glEnable(GL11.GL_COLOR_MATERIAL);
             GL11.glAlphaFunc(GL11.GL_GREATER, 0.4f);
-           
             GL11.glEnable(GL11.GL_TEXTURE_2D);
             GL11.glDisable(GL11.GL_LIGHTING); 
             GL11.glDisable(GL11.GL_FOG);
             GL11.glDisable(GL11.GL_TEXTURE_2D);
             
+            // Flush white
             GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 			
+            // Log spam
 			System.out.println("Running OpenGL version ["+glGetString(GL_VERSION)+"].");
 		}
 		catch( LWJGLException lwjglException )
 		{
+			// You shall not pass...
 			throw new RuntimeException("Could not initiate LWJGL", lwjglException);
 		}
 	}
@@ -60,14 +62,16 @@ public class Platformer {
  
 	public void run()
 	{
+		// Here be game loop
 		while(running)
 		{
-			exitLogic();
-			input();
-			update();
+			exitLogic(); // Are we exciting?
+			input(); // Fetch input
+			update(); // Updates in game logic goes here
 			render();
-			Display.update();
-			Display.sync(60);
+			
+			Display.update(); // Tell OpenGL to update
+			Display.sync(60); // Keep dat frame rate constant
 			
 		}
  
@@ -81,7 +85,12 @@ public class Platformer {
 	}
 	
 	public void input(){
-		//Handle input, duh
+		if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+			// Draw a penis!
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			// Do a barrel roll!
+		}
 	}
 	
 	public void update(){
@@ -94,6 +103,6 @@ public class Platformer {
 	 
 	public static void main(String[] args) {
 		Platformer platformerTest = new Platformer();
-		platformerTest.initAndRun("Jalla jalla",800,600);
+		platformerTest.initAndRun("Platformer Test",800,600);
 	}
 }
